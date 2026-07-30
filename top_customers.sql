8bf29eb13a8934f074bb29a814a095b2a6f0359f
mohammedsaleemshaik <shaikmd.saleem005@gmail.com>
2026-07-30 23:05:32 +0530

    Add top_customers SQL

---
+++ top_customers.sql
@@ -0,0 +1,8 @@
+SELECT
+    o.customer_id,
+    SUM(p.payment_value) AS total_spent,
+    RANK() OVER (ORDER BY SUM(p.payment_value) DESC) AS customer_rank
+FROM `ecommerce-sales-dashboard.ecommerce.orders` o
+JOIN `ecommerce-sales-dashboard.ecommerce.payments` p
+    ON o.order_id = p.order_id
+GROUP BY o.customer_id
